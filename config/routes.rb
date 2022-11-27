fd0050235be25e4c515d4727b828635fb2fa1aab
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-27 15:07:28 +0100

    Add categories dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
-  resources :groups, except: [:show, :update, :edit] do
+  resources :groups, except: [:show] do
     resources :purchases, except: [:show, :update, :edit]
   end
   devise_for :users, path: ''
